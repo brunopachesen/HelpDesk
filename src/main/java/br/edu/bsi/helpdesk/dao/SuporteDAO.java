9598b16599eb8808851e2e3ee7d8304984189b88
brunopachesen <brunopachesen@gmail.com>
2016-07-03 12:30:28 -0300

    Exclusão das classes Suporte e Usuario e inserção da variavel tipo em Funcionario; Inclusão de funções de alteração de status do chamado

--- src/main/java/br/edu/bsi/helpdesk/dao/SuporteDAO.java
+++
@@ -1,7 +0,0 @@
-package br.edu.bsi.helpdesk.dao;
-
-import br.edu.bsi.helpdesk.domain.Suporte;
-
-public class SuporteDAO extends GenericDAO<Suporte>{
-
-}
--- src/main/java/br/edu/bsi/helpdesk/dao/UsuarioDAO.java
+++
@@ -1,7 +0,0 @@
-package br.edu.bsi.helpdesk.dao;
-
-import br.edu.bsi.helpdesk.domain.Usuario;
-
-public class UsuarioDAO extends GenericDAO<Usuario>{
-
-}
